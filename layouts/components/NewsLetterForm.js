59a79ed0dd9eafb75fa7064cfc76b2bf1ff0f077
0xKrat0s <amalpkkratos@gmail.com>
2024-09-12 20:03:17 +0530

    Removed NewLetter

--- layouts/components/NewsLetterForm.js
+++ layouts/components/NewsLetterForm.js
@@ -6,32 +6,6 @@ function CustomForm({ status, message, onValidated }) {
 
   return (
     <>
-      <form
-        action="https://click.us14.list-manage.com/subscribe/post?u=d9904c2c81e977ee73bf874f7&amp;id=e3c3beb5ce"
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        className="py-6"
-      >
-        <input type="hidden" name="u" value="d9904c2c81e977ee73bf874f7" />
-        <input type="hidden" name="id" value="e3c3beb5ce" />
-        <input type="hidden" name="ht" value="97e7f7c64f4ea5f559b27b7c523dc58773a40117" />
-        <input type="hidden" name="mc_signupsource" value="hosted" />
-
-        <fieldset className="relative">
-          <input
-            className="newsletter-input form-input h-12 w-full rounded-3xl border-none bg-theme-light px-5 py-3 pr-12 text-dark placeholder:text-xs dark:bg-darkmode-theme-dark"
-            type="email"
-            name="EMAIL"  // Ensure these match Mailchimp's requirements
-            id="mce-EMAIL"
-            placeholder="Type your email and hit enter"
-          />
-          <FaEnvelope className="absolute top-1/2 right-5 -translate-y-1/2 text-xl transition duration-75" />
-        </fieldset>
-        <button className="d-block btn btn-primary mt-4 w-full" type="submit">
-          Subscribe
-        </button>
-      </form>
 
       {/* Status and message display are also removed */}
     </>
