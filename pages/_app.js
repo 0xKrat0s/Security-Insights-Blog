495840b806555c1d1fd69c94228d660ca3f69a8b
Amal PK <105988204+0xKrat0s@users.noreply.github.com>
2024-09-12 22:43:05 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import "styles/style.scss";
+import config from "@config/config.json";
+import theme from "@config/theme.json";
 import { JsonContext } from "context/state";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import TagManager from "react-gtm-module";
-import config from "@config/config.json";
-import theme from "@config/theme.json";
+import "styles/style.scss";
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter();
+  // default theme setup
   const { default_theme } = config.settings;
+
+  // import google font css
   const pf = theme.fonts.font_family.primary;
   const sf = theme.fonts.font_family.secondary;
   const [fontcss, setFontcss] = useState();
-
   useEffect(() => {
     fetch(
       `https://fonts.googleapis.com/css2?family=${pf}${
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps }) => {
     ).then((res) => res.text().then((css) => setFontcss(css)));
   }, [pf, sf]);
 
-  // Google Tag Manager
+  // google tag manager (gtm)
   const tagManagerArgs = {
     gtmId: config.params.tag_manager_id,
   };
@@ -33,22 +33,13 @@ const App = ({ Component, pageProps }) => {
         config.params.tag_manager_id &&
         TagManager.initialize(tagManagerArgs);
     }, 5000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  // Google Analytics setup
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.gtag('config', 'G-H7FJGERNB4', { page_path: url });
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <JsonContext>
       <Head>
+        {/* google font css */}
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -59,23 +50,10 @@ const App = ({ Component, pageProps }) => {
             __html: `${fontcss}`,
           }}
         />
+        {/* responsive meta */}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5"
-        />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-H7FJGERNB4`}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-H7FJGERNB4');
-          `,
-          }}
         />
       </Head>
       <ThemeProvider attribute="class" defaultTheme={default_theme}>
